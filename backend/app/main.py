d31a64525702f4e56224ccc9a23fe8878b837ca1
Sharvanee Naru <sharvanee1naru@gmail.com>
2026-03-28 13:55:36 +0530

    feat: Updated the main

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,9 +5,7 @@ from app.routes.scan_route import router as scan_router
 from app.routes.evaluation_route import router as evaluation_router   
 from app.routes.approval_route import router as approval_router   
 
-
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,11 +14,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.post("/scan")
-def scan(data: dict):
-    return {"status": "success", "data": data}
 
 app.include_router(scan_router)   
 app.include_router(evaluation_router)  
